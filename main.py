6d843101ee111655a9f07f41096e09655c4b2f09
aristapanell-cell <aristapanell@gmail.com>
2026-06-27 01:15:12 +0330

    Update main.py

--- main.py
+++ main.py
@@ -278,12 +278,11 @@ class TelegramSender:
         return {"inline_keyboard": kb}
 
     def create_caption(self, proxies: List[Tuple[str, str]]) -> str:
-        return """ 🅿🆁🅾🆇🆈
+    return """🅿🆁🅾🆇🆈
 ➖➖➖➖➖➖➖➖
 🛜 پروکسی‌های جدید.
 ✅ برای اتصال به پروکسی‌ها از دکمه‌های زیر استفاده کنید.
-👈 دسترسی به جدیدترین کانفیگ‌ها
-<a href="https://t.me/aristapanel/46625">https://t.me/aristapanel/46625</a>
+<blockquote>👈 <a href="https://t.me/aristapanel/46625">دسترسی به جدیدترین کانفیگ‌ها</a></blockquote>
 ➖➖➖➖➖➖➖➖
 <blockquote>@aristapanel</blockquote>
 ➖➖➖➖➖➖➖➖
