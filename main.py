ba085157badf0314dc7e9aaed9eba141a482eefb
aristapanell-cell <aristapanell@gmail.com>
2026-08-01 20:03:27 +0330

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ if not BOT_TOKEN:
 
 CHANNELS = [
     "https://t.me/s/hddify",
+    "https://t.me/s/khabari_18",
     "https://t.me/s/best_internet_iran",
     "https://t.me/s/times_config",
     "https://t.me/s/ProxyAnonymous",
